98761a4874d3be1417f64e75d4233dee1ced3907
jesus <200300592@ucaribe.edu.mx>
2025-05-28 22:06:48 -0500

    Fix: Added missing openai dependency

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,5 +1,5 @@
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
-require("dotenv").config
+require("dotenv").config()
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
